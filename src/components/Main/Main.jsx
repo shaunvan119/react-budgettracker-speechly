e1faa1d7e6c58e46153b0a41104c5bbb8e61eafa
shaunvan119 <shaunvan@live.com.au>
2021-11-24 15:18:32 +1100

    form and from styles component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
+
 
 const Main = () => {
     const classes = useStyles();
@@ -13,6 +15,7 @@ const Main = () => {
             <Typography align="centre" variant="h5">Total Balance $100</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5rem', marginTop: '20px'}}></Typography>
             <Divider/>
+            <Form/>
         </CardContent>
              <CardContent className={classes.CardContent}>
              <Grid container spacing={2}>
